4bd2146ca3fd05277a4c0ab554adf20de5b867df
RESTART1025 <RESTART1025@163.com>
2017-06-22 15:38:30 +0800

    宿舍

--- AlumniRecord/src/main/java/cn/edu/cumt/controller/PersonController.java
+++ AlumniRecord/src/main/java/cn/edu/cumt/controller/PersonController.java
@@ -10,9 +10,10 @@ import cn.edu.cumt.domain.Person;
 import cn.edu.cumt.service.PersonService;
 
 /**
- * 处理图书请求控制器
+ * 处理人员请求
  * */
 @Controller
+@RequestMapping(value="/person")
 public class PersonController {
 	
 	/**
@@ -26,13 +27,13 @@ public class PersonController {
 	 * 处理/main请求
 	 * */
 	@RequestMapping(value="/loginForm")
-	 public String main(Model model, String personId, String password){
-		// 获得所有图书集合
-		Person person = personService.getByPersonId(personId);
+	public String main(Model model, String personId, String password){
+		// 根据编号查询人员
+		Person person = personService.getByPersonId("123456");
 		// 将集合添加到model当中
-		model.addAttribute("book_list", "");
+		model.addAttribute("personName", person.getPersonName());
 		// 跳转到main页面
-		return "main";
+		return "loginForm";
 	}
 	
 }
